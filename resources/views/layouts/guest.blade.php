09406ab8216f48fd0f81278a20fe93a5452267bd
Austin Bouley <bouleyaustin@gmail.com>
2022-02-21 09:21:06 -0600

    Add Tracking

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="facebook-domain-verification" content="y38cjyl5xib4z6gnu8nzo8gztryv9k" />
 
         <title>SearchPod.io</title>
         <meta name="keywords" content="podcast automation, podcast search engine, podcast seo, podcast seo automation, podcast growth, podcast seo software, podcast software, podcast seo landing page, podcast episode seo, podcast episode page">
@@ -24,6 +25,35 @@
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BSGLQS1QNW"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-BSGLQS1QNW');
+        </script>
+
+        <!-- Meta Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1286096231889085');
+            fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+                       src="https://www.facebook.com/tr?id=1286096231889085&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Meta Pixel Code -->
+
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
